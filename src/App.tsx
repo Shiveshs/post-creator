c8ed57cf6b85b43e72ba45367f921f694cdb6833
Shivesh Shandilya <shivesh.shandilya@gmail.com>
2024-04-25 23:59:08 +0530

    added typesafety

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import "./App.css";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, MeshProps } from "@react-three/fiber";
 import { BoxGeometry, MeshStandardMaterial, Mesh } from "three";
 
-function App() {
-  return (<Canvas>
-    <mesh>
-      <boxGeometry args={[1,1,1]}/>
-      <meshStandardMaterial/>
-    </mesh>
-    </Canvas>);
+// Define props interface for your component
+interface AppProps {}
+
+// Define state interface if needed
+interface AppState {}
+
+// Define props for the Mesh component
+interface CustomMeshProps extends MeshProps {}
+
+// App component
+function App({}: AppProps) {
+  return (
+    <Canvas>
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} /> {/* Correct JSX syntax for boxGeometry */}
+        <meshStandardMaterial /> {/* Correct JSX syntax for meshStandardMaterial */}
+      </mesh>
+    </Canvas>
+  );
 }
 
 export default App;
