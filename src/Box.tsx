7b16ef0daf54485116301f407c5f810b135db2d3
Shivesh Shandilya <shivesh.shandilya@gmail.com>
2024-05-04 23:53:51 +0530

    Added smoother transition

--- src/Box.tsx
+++ src/Box.tsx
@@ -1,27 +1,33 @@
-import * as THREE from 'three'
-import { useRef, useState } from 'react'
-import { useFrame, ThreeElements } from '@react-three/fiber'
-
+import * as THREE from 'three';
+import { useRef, useState } from 'react';
+import { useFrame, useThree, ThreeElements} from '@react-three/fiber';
 
 const Box = (props: ThreeElements['mesh']) => {
-
-    const meshRef = useRef<THREE.Mesh>(null!);
+    const meshRef = useRef<THREE.Mesh>(null!);;
     const [hovered, setHover] = useState(false);
     const [active, setActive] = useState(false);
-    useFrame((state,delta)=>(meshRef.current.rotation.x += delta))
-  return (
-    <mesh
-    {...props}
-    ref={meshRef}
-    scale={active ? 2 : 1}
-    onClick = {()=> setActive(!active)}
-    onPointerOver={()=> setHover(true)}
-    onPointerOut={()=> setHover(false)}
-    >
-        <boxGeometry args={[2,2,2]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'red'} />
-    </mesh>
-  )
-}
+    const { clock } = useThree();
+
+    // Rotate the box smoothly
+    useFrame(() => {
+        const elapsedTime = clock.getElapsedTime();
+        meshRef.current.rotation.x = elapsedTime;
+        meshRef.current.rotation.y = elapsedTime;
+    });
+
+    return (
+        <mesh
+            {...props}
+            ref={meshRef}
+            scale={active ? 2 : 1}
+            onClick={() => setActive(!active)}
+            onPointerOver={() => setHover(true)}
+            onPointerOut={() => setHover(false)}
+        >
+            <boxGeometry args={[2, 2, 2]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'red'} />
+        </mesh>
+    );
+};
 
 export default Box;
